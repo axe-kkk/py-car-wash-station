ca0e38d2a04a8042269281f427640438ae223cdf
Maksym Kaptsov <makskaptsov13@gmail.com>
2024-09-19 17:33:29 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,47 @@
 class Car:
-    # write your code here
-    pass
+    def __init__(self, comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self, distance_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
+        self.distance_city_center = distance_city_center
+        self.clean_power = clean_power
+        self.average_rating = average_rating
+        self.count_of_ratings = count_of_ratings
+
+    def serve_cars(self, car_list: list[Car]) -> float:
+        income = 0
+        for car in car_list:
+            if car.clean_mark < self.clean_power:
+                income += self.calculate_washing_price(car)
+                self.wash_single_car(car)
+
+        return round(income, 1)
+
+    def calculate_washing_price(self, car: Car) -> float:
+        point_power_mark = self.clean_power - car.clean_mark
+        point_rating_distance = self.average_rating / self.distance_city_center
+        income = car.comfort_class * point_power_mark * point_rating_distance
+
+        return round(income, 1)
+
+    def wash_single_car(self, car: Car) -> float:
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
+        return self.calculate_washing_price(car)
+
+    def rate_service(self, rate: int) -> None:
+        if rate < 1 or rate > 5:
+            raise ValueError("Rate must be between 1 and 5")
+
+        total_rating = self.average_rating * self.count_of_ratings + rate
+        self.count_of_ratings += 1
+        self.average_rating = round(total_rating / self.count_of_ratings, 1)
